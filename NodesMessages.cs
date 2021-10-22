9454995f183a3f0f797f0236d7a9de0e714d2862
Narve Sætre <narve@dv8.no>
2021-10-22 09:00:49 +0200

    add a standard string used when specifying messages version

--- NodesMessages.cs
+++ NodesMessages.cs
@@ -5,6 +5,11 @@ namespace Nodes.Messages
     public class NodesMessages
     {
         /// <summary>
+        /// Standard string to specify messages version, used in e.g. Kafka headers
+        /// </summary>
+        public const string NodesMessagesVersionParam = "NODES.Messages.Version";
+        
+        /// <summary>
         /// NodesMessages semantic version, read from assembly 
         /// </summary>
         public static Version Version { get; } = typeof(NodesMessages).Assembly.GetName().Version;
