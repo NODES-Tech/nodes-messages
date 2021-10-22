91652c774504a86c8aa8b40197bf93a70cdef311
Narve Sætre <narve@dv8.no>
2021-10-22 08:59:04 +0200

    add version information

---
+++ NodesMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nodes.Messages
+{
+    public class NodesMessages
+    {
+        /// <summary>
+        /// NodesMessages semantic version, read from assembly 
+        /// </summary>
+        public static Version Version { get; } = typeof(NodesMessages).Assembly.GetName().Version;
+    }
+}
